8eab302a3700f3322c360d7871342b5a64425b52
Eva Honders <emhonders@gmail.com>
2016-08-31 17:32:25 +0200

    controles, asp

---
+++ Controles/Controles/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Controles.Startup))]
+namespace Controles
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
