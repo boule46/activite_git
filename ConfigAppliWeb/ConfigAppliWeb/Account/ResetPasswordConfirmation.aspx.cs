dbae416a15342ceb075be9f592373ab8fb30637f
Eva Honders <emhonders@gmail.com>
2016-09-05 22:21:23 +0200

    ConfigAppliWeb

---
+++ ConfigAppliWeb/ConfigAppliWeb/Account/ResetPasswordConfirmation.aspx.cs
@@ -0,0 +1,8 @@
+﻿using System.Web.UI;
+
+namespace ConfigAppliWeb.Account
+{
+    public partial class ResetPasswordConfirmation : Page
+    {
+    }
+}
---
+++ ConfigAppliWeb/ConfigAppliWeb/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ConfigAppliWeb.Global" Language="C#" %>
