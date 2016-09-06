adac0138f30645b4a003c40679461eaba61abf2b
Eva Honders <emhonders@gmail.com>
2016-09-06 13:55:02 +0200

    global.aspx.cs
    evenements session, application, compteur request avec lock

--- ConfigAppliWeb/ConfigAppliWeb/Default.aspx.cs
+++ ConfigAppliWeb/ConfigAppliWeb/Default.aspx.cs
@@ -9,9 +9,23 @@ namespace ConfigAppliWeb
 {
     public partial class _Default : Page
     {
+
+       
+
+       
+
         protected void Page_Load(object sender, EventArgs e)
         {
+            Label1.Text = string.Format("ID de session : {0}.\n", this.Session.SessionID);
+            Label2.Text = string.Format("Date et heure de session : {0}\n", Session["SessionHeure"].ToString());
+            //Label3.Text = "Date et heure du context : " + Context.Items["ContextHeure"].ToString();
+            Label4.Text = "Date et heure de l'application : " + Application["AppliHeure"].ToString();
+            Label3.Text = Application["CompteurRequest"].ToString();
+           
+
 
         }
+
+
     }
 }
--- ConfigAppliWeb/ConfigAppliWeb/Global.asax.cs
+++ ConfigAppliWeb/ConfigAppliWeb/Global.asax.cs
@@ -6,6 +6,7 @@ using System.Web.Optimization;
 using System.Web.Routing;
 using System.Web.Security;
 using System.Web.SessionState;
+using System.Threading;
 
 namespace ConfigAppliWeb
 {
@@ -13,9 +14,34 @@ namespace ConfigAppliWeb
     {
         void Application_Start(object sender, EventArgs e)
         {
+            Application["AppliHeure"] = DateTime.Now;
+           
             // Code that runs on application startup
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
+
+        void Session_Start(object sender, EventArgs e)
+        {
+            Session["SessionHeure"] = DateTime.Now;
+
+           
+
+        }
+
+        void Context_Start(object sender, EventArgs e)
+        {
+            Context.Items["ContextHeure"] = DateTime.Now;
+        }
+
+        void Application_PostResolveRequestCache(object sender, EventArgs e)
+        {
+            Application.Lock();
+            Application["CompteurRequest"] = (Application["CompteurRequest"] != null) ? ((int)Application["CompteurRequest"]) + 1 : 1;
+            Application.UnLock();
+        }
+
+       
+        
     }
 }
